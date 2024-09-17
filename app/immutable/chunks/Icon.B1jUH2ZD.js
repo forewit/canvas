6aaa3df8175b3df261f1c8526902b827ae8da830
Marc Anderson <marcda495@gmail.com>
2024-09-17 13:54:51 -0400

    Updates

--- app/immutable/chunks/Icon.B1jUH2ZD.js
+++
@@ -1,5 +0,0 @@
-import{b as o,aq as c,O as f,$ as l,h as p,f as m,m as v,n as d,v as u}from"./runtime.4vCN5gQe.js";import{a as h,t as b}from"./disclose-version.CBUhcYe9.js";import{s as _}from"./attributes.s4vI7vNd.js";import{p as i}from"./index-client.CJwHoa7I.js";function w(n,t,...r){var s=n,a,e;o(()=>{a!==(a=t())&&(e&&(l(e),e=null),a&&(e=f(()=>a(s,...r))))},c),p&&(s=m)}var g=b('<div class="icon svelte-cvf5gt"><span class="svelte-cvf5gt"></span></div>');function z(n,t){let r=i(t,"size",3,"1em"),s=i(t,"color",3,"var(--main)");var a=g(),e=v(a);d(a),u(()=>_(e,"style",`-webkit-mask: url(${t.url??""}) no-repeat center / contain;
-        mask: url(${t.url??""}) no-repeat center / contain; 
-        width: ${r()??""}; 
-        height: ${r()??""}; 
-        background-color: ${s()??""};`)),h(n,a)}export{z as I,w as s};
---
+++ app/immutable/entry/start.Bn2Uz21j.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DXY5q1wP.js";export{t as start};

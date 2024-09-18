f15083976e286697c260668e78fe57c573691e93
Marc Anderson <marcda495@gmail.com>
2024-09-18 16:25:08 -0400

    Updates

---
+++ app/immutable/chunks/Icon.r047pPmM.js
@@ -0,0 +1,5 @@
+import{a as n,t as l}from"./disclose-version.zPFKPM9w.js";import{M as m,Q as c,R as p}from"./runtime.BmmPnVCB.js";import{s as v}from"./attributes.BgxIPSn9.js";import{p as r}from"./props.D67dsuGD.js";var d=l('<div class="icon svelte-1m7i2ot"><span class="svelte-1m7i2ot"></span></div>');function b(o,t){let a=r(t,"size",3,"1em"),s=r(t,"color",3,"var(--main)");var e=d(),i=c(e);p(e),m(()=>v(i,"style",`-webkit-mask: url(${t.url??""}) no-repeat center / contain;
+        mask: url(${t.url??""}) no-repeat center / contain; 
+        width: ${a()??""}; 
+        height: ${a()??""}; 
+        background-color: ${s()??""};`)),n(o,e)}export{b as I};
--- app/immutable/chunks/paths.DXh74Bk7.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_w1t3xt)==null?void 0:s.base)??"/canvas";var t;const e=((t=globalThis.__sveltekit_w1t3xt)==null?void 0:t.assets)??a;export{e as a,a as b};
